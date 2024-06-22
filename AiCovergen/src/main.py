715ca2051b8b5c906c816c427d98e72bea7b7e4c
Nex432 <167639579+Nex432@users.noreply.github.com>
2024-06-22 09:43:40 +0700

    Update main.py

--- AiCovergen/src/main.py
+++ AiCovergen/src/main.py
@@ -7,6 +7,9 @@ import shlex
 import subprocess
 from contextlib import suppress
 from urllib.parse import urlparse, parse_qs
+from pathlib import Path
+import requests
+
 
 import gradio as gr
 import librosa
@@ -26,6 +29,44 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 mdxnet_models_dir = os.path.join(BASE_DIR, 'mdxnet_models')
 rvc_models_dir = os.path.join(BASE_DIR, 'rvc_models')
 output_dir = os.path.join(BASE_DIR, 'song_output')
+
+
+
+MDX_DOWNLOAD_LINK = 'https://github.com/TRvlvr/model_repo/releases/download/all_public_uvr_models/'
+RVC_DOWNLOAD_LINK = 'https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/'
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+def dl_model(link, model_name, dir_name):
+    with requests.get(f'{link}{model_name}') as r:
+        r.raise_for_status()
+        with open(dir_name / model_name, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+
+if __name__ == '__main__':
+    mdx_model_names = ['UVR-MDX-NET-Voc_FT.onnx', 'UVR_MDXNET_KARA_2.onnx', 'Reverb_HQ_By_FoxJoy.onnx', 'Kim_Vocal_2.onnx']
+    for model in mdx_model_names:
+        model_path = mdxnet_models_dir / model
+        if model_path.exists():
+            print(f'{model} already exists, skipping download.', flush=True)
+        else:
+            print(f'Downloading {model}...', flush=True)
+            dl_model(MDX_DOWNLOAD_LINK, model, mdxnet_models_dir)
+
+    rvc_model_names = ['hubert_base.pt', 'rmvpe.pt']
+    for model in rvc_model_names:
+        model_path = rvc_models_dir / model
+        if model_path.exists():
+            print(f'{model} already exists, skipping download.', flush=True)
+        else:
+            print(f'Downloading {model}...', flush=True)
+            dl_model(RVC_DOWNLOAD_LINK, model, rvc_models_dir)
+
+    print('All models downloaded!', flush=True)
 
 
 def get_youtube_video_id(url, ignore_playlist=True):
